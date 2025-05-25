b0609acfe680e8b00a79323999732d3f2205e2ff
Dat_Nghiem <nghiemdat2708@gmail.com>
2025-05-25 09:29:19 +0700

    Initial commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <NuxtRouteAnnouncer />
+<!--    <NuxtWelcome />-->
+    <NuxtPage />
+  </div>
+</template>
+
+<!--<template>-->
+<!--  <div class="min-h-screen bg-gray-100 flex items-center justify-center">-->
+<!--    <div class="bg-white p-8 rounded-lg shadow-md">-->
+<!--      <h1 class="text-3xl font-bold text-blue-600 mb-4">-->
+<!--        Hello Nuxt + Tailwind!-->
+<!--      </h1>-->
+<!--      <p class="text-gray-600">-->
+<!--        Project của bạn đã sẵn sàng!-->
+<!--      </p>-->
+<!--      <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">-->
+<!--        Click me-->
+<!--      </button>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
