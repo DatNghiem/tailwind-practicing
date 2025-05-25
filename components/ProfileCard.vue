06d0e08c11b5d741b5339b9b401631bf058ee476
Dat_Nghiem <nghiemdat2708@gmail.com>
2025-05-25 10:21:38 +0700

    Initial commit

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="max-w-sm mx-auto bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden md:max-w-2xl transition hover:scale-105">
-    <div class="md:flex">
+    <div class="md:flex items-stretch">
       <div class="md:shrink-0">
-        <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://i.pravatar.cc/150?img=3" alt="Avatar">
+        <img class="h-48 w-full object-cover md:h-full md:w-48" src="@/assets/images/pexels-julian-dahl-2149417377-31353860.jpg" alt="Avatar">
       </div>
       <div class="p-8">
         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Software Engineer</div>
-        <h1 class="block mt-1 text-lg leading-tight font-medium text-black dark:text-white">Đạt Nguyễn</h1>
+        <h1 class="block mt-1 text-lg leading-tight font-medium text-black dark:text-white">Đạt Nghiêm</h1>
         <p class="mt-2 text-gray-500 dark:text-gray-300">
           Đam mê xây dựng sản phẩm ý nghĩa với Tailwind, Vue và một chút JavaScript magic ✨
         </p>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss'
   ],
   css: ['~/assets/css/main.css'],
-  typescript: {
-    typeCheck: true
-  }
+  // typescript: {
+  //   typeCheck: true
+  // }
 })
